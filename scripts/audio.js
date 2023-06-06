4274d705135b04536d0226317b858c36923ab3c7
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2023-06-05 18:40:45 -0700

    audio loops

---
+++ scripts/audio.js
@@ -0,0 +1,10 @@
+var sound = new Howl({
+    src: ['https://eden-annora.github.io/TheNautilus/audio/ambientmusic/magic_forest.mp3'],
+    loop:true
+  });
+  
+
+  sound.play();
+
+  
+  sound.resume();
