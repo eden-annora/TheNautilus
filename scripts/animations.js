29b74535e122af37fd9df6473b82ef31a5895d42
eden-annora <133988751+eden-annora@users.noreply.github.com>
2023-06-08 17:57:55 -0700

    Update animations.js

--- scripts/animations.js
+++ scripts/animations.js
@@ -31,8 +31,93 @@ const animations = new Object({
             ctx.beginPath();
             ctx.arc(tpX, tpY, (framecounter * 8 / 30) * 100, 0, 2 * Math.PI);
             ctx.stroke();
+
+            ctx.strokeStyle = "#34ebba15"
+            ctx.lineWidth = 30;
+
+            if (framecounter < 30) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 100, (framecounter / 10) * Math.PI, (framecounter / 5) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 35) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 105, (framecounter / 15) * Math.PI, (-framecounter / 10) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 40) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 110, (framecounter / 20) * Math.PI, (framecounter / 15) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 43) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 115, (framecounter / 25) * Math.PI, (-framecounter / 19.8) * Math.PI);
+                ctx.stroke();
+            }
+            ctx.beginPath();
+            ctx.arc(tpX, tpY, (framecounter * 8 / 30) * 100, 0, 2 * Math.PI);
+            ctx.stroke();
+
         }
     },
+    "player_StoreMomentum_idle": function (pos, framecounter) {
+        ctx.strokeStyle = "#e06fff"
+        ctx.lineWidth = 1;
+        if (framecounter < 50) {
+
+            let tpX = centerOfCanvas.X + (pos.X - camera.pos.X) // set transforms for the center of the canvas, the image width, and cameras relitave position to the player.
+            let tpY = centerOfCanvas.Y + (pos.Y - camera.pos.Y)
+
+
+            if (framecounter < 30) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 100, (framecounter / 10) * Math.PI, (framecounter / 5) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 35) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 105, (framecounter / 15) * Math.PI, (-framecounter / 10) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 40) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 110, (framecounter / 20) * Math.PI, (framecounter / 15) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 43) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 115, (framecounter / 25) * Math.PI, (-framecounter / 20) * Math.PI);
+                ctx.stroke();
+            }
+
+
+            ctx.strokeStyle = "#e06fff15"
+            ctx.lineWidth = 30;
+            if (framecounter < 30) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 100, (framecounter / 10) * Math.PI, (framecounter / 5) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 35) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 105, (framecounter / 15) * Math.PI, (-framecounter / 10) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 40) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 110, (framecounter / 20) * Math.PI, (framecounter / 15) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 43) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 115, (framecounter / 25) * Math.PI, (-framecounter / 20) * Math.PI);
+                ctx.stroke();
+            }
+    
+        }
+    },
+
     "player_StoreMomentum": function (pos, framecounter) {
         ctx.strokeStyle = "#e06fff"
         ctx.lineWidth = 1;
@@ -62,9 +147,40 @@ const animations = new Object({
                 ctx.arc(tpX, tpY, 115, (framecounter / 25) * Math.PI, (-framecounter / 20) * Math.PI);
                 ctx.stroke();
             }
+            if (framecounter < 25) {
             ctx.beginPath();
-            ctx.arc(tpX, tpY, Math.abs(((3660 - framecounter * 32))), 0, 2 * Math.PI);
+            ctx.arc(tpX, tpY, Math.abs(((500 - framecounter * 16))), 0, 2 * Math.PI);
             ctx.stroke();
+            }
+
+            ctx.strokeStyle = "#e06fff15"
+            ctx.lineWidth = 30;
+            if (framecounter < 30) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 100, (framecounter / 10) * Math.PI, (framecounter / 5) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 35) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 105, (framecounter / 15) * Math.PI, (-framecounter / 10) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 40) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 110, (framecounter / 20) * Math.PI, (framecounter / 15) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 43) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, 115, (framecounter / 25) * Math.PI, (-framecounter / 20) * Math.PI);
+                ctx.stroke();
+            }
+            if (framecounter < 25) {
+                ctx.beginPath();
+                ctx.arc(tpX, tpY, Math.abs(((500 - framecounter * 16))), 0, 2 * Math.PI);
+                ctx.stroke();
+                }
+    
         }
     }
 })
